c531d4812d0281bf79a336894f191ee3299f952f
Akhil_Vinayak_P <vinayakakhil030@gmail.com>
2024-05-21 21:39:59 +0530

    added some fetures

--- shop/models.py
+++ shop/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+class Product(models.Model):
+    product_id = models.AutoField
+    product_name=models.CharField(max_length=50)
+    category = models.CharField(max_length=100,default="")
+    subcategory = models.CharField(max_length=100,default="")
+    price=models.FloatField(max_length=10,default=0.0)
+    image=models.ImageField(upload_to="shop/images",default="")
+    desc=models.CharField(max_length=300)
+    pub_date=models.DateField()
+
+    def __str__(self):
+        return self.product_name
--- shop/views.py
+++ shop/views.py
@@ -2,4 +2,22 @@ from django.shortcuts import  render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("index Shop")
+    return render(request,'shop/index.html')
+
+def about(request):
+    return HttpResponse("tHIS IS MY ABOUT FUNCTION")
+
+def contact(request):
+    return HttpResponse("Contact Information:7785789456")
+
+def tracker(request):
+    return HttpResponse("Track Id")
+
+def search(request):
+    return HttpResponse("Search Information:")
+
+def productView(request):
+    return HttpResponse("Product Information:")
+
+def checkout(request):
+    return HttpResponse("Checkout Information:")
